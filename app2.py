ec0e276ce3b7c9d8fb5f7ffae6e2928a66c211bb
Mohammad Shehzar Khan <135840484+shezkh@users.noreply.github.com>
2023-11-05 17:36:18 +0300

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,42 @@
+import numpy as np
+from flask import Flask, request, render_template, jsonify
+import pickle
+
+app = Flask(__name)
+
+model = pickle.load(open('model2.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    prediction = float(prediction[0])
+    output = round(prediction, 2)
+
+    return render_template('index.html', prediction_text='Salary should be $ {}'.format(output))
+
+@app.route('/api/predict', methods=['POST'])
+def api_predict():
+    try:
+        data = request.get_json()
+        # Assuming you expect an array of input values as 'features'
+        features = data['features']
+        input_features = [int(x) for x in features]
+        final_features = [np.array(input_features)
+        prediction = model.predict(final_features)
+
+        prediction = float(prediction[0])
+        output = round(prediction, 2)
+
+        return jsonify({"prediction": output})
+    except Exception as e:
+        return jsonify({"error": "Invalid input data"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
