a1ebdaf450bbd94c571b7632192c6e162a2d88cf
Mohammad Shehzar Khan <135840484+shezkh@users.noreply.github.com>
2023-11-05 17:40:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,22 @@ def predict():
     
     return render_template('index.html', prediction_text='Salary should be $ {}'.format(output))
 
+@app.route('/api/predict', methods=['POST'])
+def api_predict():
+    try:
+        data = request.get_json()
+        # Assuming you expect an array of input values as 'features'
+        features = data['features']
+        input_features = [int(x) for x in features]
+        final_features = [np.array(input_features)
+        prediction = model.predict(final_features)
+
+        prediction = float(prediction[0])
+        output = round(prediction, 2)
+
+        return jsonify({"prediction": output})
+    except Exception as e:
+        return jsonify({"error": "Invalid input data"})
+
 if __name__ == "__main__":
     app.run(debug=True)
